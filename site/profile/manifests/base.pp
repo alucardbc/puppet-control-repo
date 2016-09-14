e680ed19512ac939e80ca4192014fc7b29ddaa52
alucardbc <alucardbc@users.noreply.github.com>
2016-09-14 11:09:56 -0600

    Create base.pp

---
+++ site/profile/manifests/base.pp
@@ -0,0 +1,21 @@
+class profile::base {
+  user { 'root':
+    ensure           => 'present',
+    comment          => 'root',
+    gid              => '0',
+    home             => '/root',
+    password         => '$1$n/kUYSYj$1ogOgMSfoDfM0uYhWACmv1',
+    shell            => '/bin/bash',
+    uid              => '0',
+  }
+  
+  user { 'ddewey':
+    ensure => 'present',
+  }
+  
+  include ntp_wrapper
+  
+  package { 'vim':
+    ensure => present,
+  }
+}
