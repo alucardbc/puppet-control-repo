4a002020d3103cd8433d5c7e5bf62702d4fee858
alucardbc <alucardbc@users.noreply.github.com>
2016-09-15 11:32:51 -0600

    Update blog.pp

--- site/profile/manifests/blog.pp
+++ site/profile/manifests/blog.pp
@@ -2,15 +2,6 @@ class profile::blog {
   class { 'apache': 
     default_vhost => false,
   }
-  
-  apache::fastcgi::server { 'php':
-    host       => '127.0.0.1:9000',
-    timeout    => 15,
-    flush      => false,
-    faux_path  => '/var/www/php.fcgi',
-    fcgi_alias => '/php.fcgi',
-    file_type  => 'application/x-httpd-php'
-  }
 
   class { '::mysql::server':
     root_password           => 'strongpassword',
